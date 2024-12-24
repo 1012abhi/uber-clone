bf14b741a345a03eaeb44ef94a204bb6ae23f4f9
1012abhi <130040653+1012abhi@users.noreply.github.com>
2024-12-24 17:32:11 +0530

    Added axios request to fetch user data and updated useEffect hook to include setUser and navigate dependencies

--- Frontend/src/pages/UserProtectedWrapper.jsx
+++ Frontend/src/pages/UserProtectedWrapper.jsx
@@ -1,21 +1,35 @@
 import React, { useContext, useEffect } from 'react'
 import { UserDatacontext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 
 const UserProtectedWrapper = ({children}) => {
 
     const token = localStorage.getItem('token')
-    console.log(token);
-    
+    const { user, setUser } = useContext(UserDatacontext)
     const navigate = useNavigate()
     
     useEffect(() => {
-    if (!token) {
-        navigate('/userlogin')
-        // return null;
-    }                                    
-    
-    }, [token])
+        if (!token) {
+            navigate('/userlogin')
+            // return null;
+        }                                    
+        
+        axios.get(`${import.meta.env.VITE_BASE_URL}/api/users/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then((response) => {
+            const data = response.data;
+            setUser(data.user)
+
+
+        })
+
+
+    }, [token, navigate, setUser]);
 
     return (
        <>
