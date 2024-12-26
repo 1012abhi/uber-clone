8fdc6a25887c03bf7cc2d7cf181bfd945f92bb0b
1012abhi <130040653+1012abhi@users.noreply.github.com>
2024-12-26 23:11:17 +0530

    Refactor UserHome page: add VehiclePanel component and remove hardcoded vehicle options

--- Frontend/src/pages/UserHome.jsx
+++ Frontend/src/pages/UserHome.jsx
@@ -3,6 +3,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap"
 import 'remixicon/fonts/remixicon.css'
 import LocationSearchPanel from '../components/LocationSearchPanel';
+import VehiclePanel from '../components/VehiclePanel';
 
 function UserHome() {
   const [pickup, setPickup] = useState('')
@@ -84,36 +85,8 @@ function UserHome() {
             />
         </div>
       </div>
-      <div ref={vehiclePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-8'>
-          <h5 className='p-3 text-center'><i class="ri-arrow-down-wide-fill"></i></h5>
-          <h3 className='text-2xl font-semibold mb-5'>Choose a Vehicle</h3>
-          <div className='flex border-2 active:border-black rounded-xl w-full p-3  mb-2 items-center justify-between '>
-            <img className='h-12' src="car.png" alt="" />
-            <div className='ml-2 w-1/2'>
-              <h4 className='font-medium text-base'>UberGos <span><i className="ri-user-3-fill"></i>4</span></h4>
-              <h5 className='font-medium text-sm'>2 min away</h5>
-              <p className='font-normal text-xs text-gray-600'>Affordable, cpmpact rides</p>
-            </div>
-            <h2 className='text-lg font-semibold'>₹193.20</h2>
-          </div>
-          <div className='flex border-2 active:border-black rounded-xl w-full p-3  mb-2 items-center justify-between '>
-            <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
-            <div className='-ml-4 w-1/2'>
-              <h4 className='font-medium text-base'>Moto <span><i className="ri-user-3-fill"></i>1</span></h4>
-              <h5 className='font-medium text-sm'>3 min away</h5>
-              <p className='font-normal text-xs text-gray-600'>Affordable, motorcycle rides</p>
-            </div>
-            <h2 className='text-lg font-semibold'>₹65</h2>
-          </div>
-          <div className='flex border-2 active:border-black rounded-xl w-full p-3  mb-2 items-center justify-between '>
-            <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
-            <div className='ml-2 w-1/2'>
-              <h4 className='font-medium text-base'>UberAuto <span><i className="ri-user-3-fill"></i>3</span></h4>
-              <h5 className='font-medium text-sm'>3 min away</h5>
-              <p className='font-normal text-xs text-gray-600'>Affordable, Auto rides</p>
-            </div>
-            <h2 className='text-lg font-semibold'>₹118.lg</h2>
-          </div>
+      <div ref={vehiclePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-14'>
+          <VehiclePanel setVehiclePanel={setVehiclePanel}/>
       </div>
     </div>
   )
