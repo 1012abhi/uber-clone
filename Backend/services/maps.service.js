5532add9e5837a7b90eabbb76d077ca26c7d429d
1012abhi <130040653+1012abhi@users.noreply.github.com>
2024-12-29 15:22:14 +0530

    fetch maps api using axios

---
+++ Backend/services/maps.service.js
@@ -0,0 +1,23 @@
+const getAddressCoordinate = async (address) => {
+    const apiKey = 'YOUR_API_KEY';
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
+    
+    try {
+        const response = await axios.get(url);
+        if (response.data.status === 'OK') {
+            const location = response.data.results[ 0 ].geometry.lacation;
+            return {
+                ltd: location.lat,
+                lng: location.lng
+            }
+        } else {
+            throw new Error('Unable to fetch coordinates')
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+        
+    }
+}
+
+export default getAddressCoordinate;
