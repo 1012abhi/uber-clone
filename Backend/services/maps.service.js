a5490e0950a44317def23a196dee9b824d91c5ed
1012abhi <130040653+1012abhi@users.noreply.github.com>
2025-01-03 23:58:18 +0530

    Update maps service to handle missing API key and change Google Maps API URLs

--- Backend/services/maps.service.js
+++ Backend/services/maps.service.js
@@ -3,21 +3,30 @@ import axios from "axios";
 
 const getAddressCoordinate = async (address) => {
     const apiKey = process.env.GOOGLE_MAPS_API;
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
+    if (!apiKey) {
+        console.log('Google Maps API key is not configured');
+        throw new Error('API key is missing');
+    }
+
+    const url = `https://maps.gomaps.pro/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
     
     try {
         const response = await axios.get(url);
         if (response.data.status === 'OK') {
-            const location = response.data.results[ 0 ].geometry.location;
+            const location = response.data.results[0].geometry.location;
+            console.log('location', location);
+            
             return {
-                ltd: location.lat,
-                lng: location.lng
+                lat: location.lat,
+                lng: location.lng,
+                formatted_address: response.data.results[0].formatted_address
             }
         } else {
+            console.error('Error in API response:', response.data);
             throw new Error('Unable to fetch coordinates')
         }
     } catch (error) {
-        console.error(error);
+        console.error('Error fetching coordinates:', error.message);
         throw error;
         
     }
@@ -29,11 +38,12 @@ const getDistanceAndTime = async (origin, destination) => {
     }
 
     const apiKey = process.env.GOOGLE_MAPS_API;
-    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent}$key=${apiKey}`
-    
+    const url = `https://maps.gomaps.pro/maps/api/distancematrix/json?origins=${encodeURIComponent(origin)}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`
+
     try {
         
         const response = await axios.get(url);
+        
         if (response.data.status === 'OK') {
             
             if (response.data.rows[0].elements[0].status === 'ZERO_RESULTS') {
@@ -55,7 +65,7 @@ const getAutoCompleteSuggestions = async (input) => {
     }
 
     const apiKey = process.env.GOOGLE_MAPS_API;
-    const url = `https://maps.googleapis.com/maps/api/placeautocomplete/json?input=${encodeURIComponent}$key=${apiKey}`
+    const url = `https://maps.gomaps.pro/maps/api/placeautocomplete/json?input=${encodeURIComponent(input)}$key=${apiKey}`
 
     try {
         const response = await axios.get(url);
